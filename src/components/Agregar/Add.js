113fded1d4e8a527bccc16edd6120c32b20c83b7
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-07-17 21:54:39 -0400

    Creando el formulario para crear recetas

--- src/components/Agregar/Add.js
+++ src/components/Agregar/Add.js
@@ -1,13 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Add = () => {
+    //1. Agregar propiedad del propio componente
+    const titulo = "Añadir receta"
+    //9. Guardar esta inforamcion en el estado del propio componente
+    const [recetaState, setRecetaState] = useState({
+        title: '',
+        description: '',
+    });
+
+    const { title, description } = recetaState;
+
+    //5.
+    const conseguirDatosForm = (e) => {
+        //6. Evitar el funcionamiento por default del form(actualizar)
+        e.preventDefault();
+        //7. Conseguir datos del form
+        let target = e.target;
+        let titulo = target.title.value;
+        let description = target.description.value;
+        //8. Crear objeto de la receta a guardar
+        let receta = {
+            id: new Date().getTime(),
+            titulo,
+            description,
+        };
+
+        setRecetaState(receta)
+
+        console.log(recetaState)
+    }
+    
+
     return (
         <div className="add">
-            <h3 className="title">Añadir recetas</h3>
-            <form>
-                <input type="text" placeholder="Titulo" />
-                <textarea placeholder="descripcion"></textarea>
-                <input type="submit" value="guardar" />
+            {/*2. Sustituir variable por componente */}
+            <h3 className="title">{titulo}</h3>
+            <strong>
+                {(title && description) && "Has creado la receta: " + title}
+            </strong>
+            {/*4. Vincular al formulario un evento con onSubmit*/}
+            <form onSubmit={conseguirDatosForm}>
+                {/*3. Conseguir datos del formulario, cuando sea rellenado*/}
+                <input
+                    type="text"
+                    id='title'
+                    name='title'
+                    placeholder="Titulo" />
+                <textarea
+                    id='description'
+                    name='description'
+                    placeholder="Descripcion"></textarea>
+                <input
+                    type="submit"
+                    id='save'
+                    value="guardar" />
             </form>
         </div>
     )
